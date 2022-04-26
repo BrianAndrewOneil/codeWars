7338855310804918e911a5a9289b8b6ec0a8ef3c
BrianAndrewOneil <brian.andrew.oneil@gmail.com>
2022-04-26 19:38:19 -0400

    new code wars

---
+++ stringCleaning.js
@@ -0,0 +1,13 @@
+function stringClean(str){
+    str = str.replace(/0/g,'');
+    str = str.replace(/1/g,'');
+    str = str.replace(/2/g,'');
+    str = str.replace(/3/g,'');
+    str = str.replace(/4/g,'');
+    str = str.replace(/5/g,'');
+    str = str.replace(/6/g,'');
+    str = str.replace(/7/g,'');
+    str = str.replace(/8/g,'');
+    str = str.replace(/9/g,'');
+    return str;
+  }
