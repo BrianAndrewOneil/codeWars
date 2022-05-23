3dd8434a74195f87e9eebf8556eb07ae94d0fd7c
BrianAndrewOneil <brian.andrew.oneil@gmail.com>
2022-05-23 17:13:48 -0400

    code wars

---
+++ youOnlyNeedOne.js
@@ -0,0 +1,3 @@
+const check = (a, x) => (a.find(element => element == x))==undefined?false:true
+
+//Given an array a and a value x. Check whether the provided array contains the value. Array can contain numbers or strings. X can be either. Return true if the array contains the value, false if not.
