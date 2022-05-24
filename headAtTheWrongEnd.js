1d0e9b0829d0e87753fe4190b944af2ad33b675d
BrianAndrewOneil <brian.andrew.oneil@gmail.com>
2022-05-24 17:39:10 -0400

    code wars

---
+++ headAtTheWrongEnd.js
@@ -0,0 +1,5 @@
+let fixTheMeerkat = arr => arr.reverse()
+//You're at the zoo... all the meerkats look weird. Something has gone terribly wrong - 
+//someone has gone and switched their heads and tails around!
+//Save the animals by switching them back. You will be given an array which will have three values (tail, body, head).
+//Rearrange the array so that the animal is the right way round (head, body, tail).
