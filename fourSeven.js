4b92d717fb7ac1fa3c290be498330a0c77fba8fd
BrianAndrewOneil <brian.andrew.oneil@gmail.com>
2022-08-10 18:31:50 -0400

    code wars

---
+++ fourSeven.js
@@ -0,0 +1,11 @@
+//When 4 is entered, return 7, and vice versa. One catch: do not use 1f statements, 
+//sw1tch statements, or the ternary operator.
+function fourSeven(n){
+    while(n==4){
+      return 7
+    }
+    while(n==7){
+      return 4
+    }
+    return 0
+  }
