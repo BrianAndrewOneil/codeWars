965a49f438b0b3d83fd1c18ec5fb55935e5d5b8a
BrianAndrewOneil <brian.andrew.oneil@gmail.com>
2022-08-29 18:20:46 -0400

    code wars

---
+++ greetMe.js
@@ -0,0 +1,2 @@
+//Takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+let greet = name => `Hello ${name.charAt(0).toUpperCase()}${name.slice(1).toLowerCase()}!`
