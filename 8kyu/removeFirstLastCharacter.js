ccca7215774190d4ac102f3fb3d70d628fad1afe
Brian O'Neil <102704305+BrianAndrewOneil@users.noreply.github.com>
2022-09-06 22:28:51 -0400

    Rename removeFirstLastCharacter.js to 8kyu/removeFirstLastCharacter.js

---
+++ 8kyu/removeFirstLastCharacter.js
@@ -0,0 +1,2 @@
+//Remove the first and last characters of a string.
+let removeChar =str => str.split('').slice(1,(str.length-1)).join('')
--- removeFirstLastCharacter.js
+++
@@ -1,2 +0,0 @@
-//Remove the first and last characters of a string.
-let removeChar =str => str.split('').slice(1,(str.length-1)).join('')
