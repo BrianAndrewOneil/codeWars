1449834d836cc3e9a47afa29668ff1863e9ea53d
Brian O'Neil <102704305+BrianAndrewOneil@users.noreply.github.com>
2022-09-06 22:27:21 -0400

    Rename findThePosition.js to 8kyu/findThePosition.js

---
+++ 8kyu/findThePosition.js
@@ -0,0 +1,2 @@
+//When provided with a letter, return its position in the alphabet.
+let position = letter => `Position of alphabet: ${letter.charCodeAt(0)-96}`
--- findThePosition.js
+++
@@ -1,2 +0,0 @@
-//When provided with a letter, return its position in the alphabet.
-let position = letter => `Position of alphabet: ${letter.charCodeAt(0)-96}`
