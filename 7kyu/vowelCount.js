ebffd30448cdddad9e20e130b3c5aa287c20db8c
Brian O'Neil <102704305+BrianAndrewOneil@users.noreply.github.com>
2022-09-06 22:23:49 -0400

    Rename vowelCount.js to 7kyu/vowelCount.js

---
+++ 7kyu/vowelCount.js
@@ -0,0 +1,2 @@
+//Return the number (count) of vowels in the given string.
+let getCount = str => str.split('').filter(e=>e=='a'||e=='e'||e=='i'||e=='o'||e=='u').length
--- vowelCount.js
+++
@@ -1,2 +0,0 @@
-//Return the number (count) of vowels in the given string.
-let getCount = str => str.split('').filter(e=>e=='a'||e=='e'||e=='i'||e=='o'||e=='u').length
