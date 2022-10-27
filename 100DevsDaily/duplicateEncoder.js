9aa07c9b5fe7e225f4533cc2214bcc39efa4c379
BrianAndrewOneil <brian.andrew.oneil@gmail.com>
2022-10-27 15:45:51 -0600

    code wars

---
+++ 100DevsDaily/duplicateEncoder.js
@@ -0,0 +1,11 @@
+//Convert a string to a new string where each character in the new string is "(" 
+//if that character appears only once in the original string, or ")" if that 
+//character appears more than once in the original string.
+function duplicateEncode(word){
+    word=word.split('')
+    //const l = 'e'
+    //return word.lastIndexOf(l)
+    return word.map((l,i,a)=>a.indexOf(l)===a.lastIndexOf(l)?'(':')').join('')
+}
+
+console.log(duplicateEncode('din'))
