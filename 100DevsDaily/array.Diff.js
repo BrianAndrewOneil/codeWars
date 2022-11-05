60a642be24bb72433d303aa27b4dc57b032e7813
BrianAndrewOneil <brian.andrew.oneil@gmail.com>
2022-11-05 19:35:39 -0400

    code wars

--- 100DevsDaily/array.Diff.js
+++ 100DevsDaily/array.Diff.js
@@ -1,3 +1,10 @@
 //Subtracts one list from another and return the result.
 //Remove all values from list a, which are present in list b keeping their order.
 let arrayDiff = (a,b) => a.filter(num=>!(b.includes(num)))
+
+//or the following, which is more efficient, linear time
+function arrayDiff(a,b){
+    if(a.length===0||b.length===0) return a
+    let newSet = new Set(b)
+    return a.filter(n=>!newSet.has(n))
+}
