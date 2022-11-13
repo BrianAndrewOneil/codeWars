282af924bfc7d5c70524a7a96d0a41ed6f68b8ed
BrianAndrewOneil <brian.andrew.oneil@gmail.com>
2022-11-12 19:29:55 -0500

    code wars

--- 100DevsDaily/findTheMiddleElement.js
+++ 100DevsDaily/findTheMiddleElement.js
@@ -1,8 +1,10 @@
 //when provided with a triplet, returns the index of the numerical element that lies between the other two elements
 function gimme (triplet) {
     let ans=triplet.map(e=>e).sort((a,b)=>a-b)
-    return triplet.indexOf(+ans[1])
+    return triplet.indexOf(ans[1])
 }
+
+//or, a one-liner would be: let gimme = triplet => arr.indexOf([...arr].sort((a,b)=>a-b)[1])
 
 console.log(gimme([2, 3, 1]), 0)
 console.log(gimme([5, 10, 14]), 1)
