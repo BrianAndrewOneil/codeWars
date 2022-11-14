d44464d860a89b8cf654c15ab262b63c6d1d453b
BrianAndrewOneil <brian.andrew.oneil@gmail.com>
2022-11-13 19:27:24 -0500

    code wars

--- 100DevsDaily/sortTheOdd.js
+++ 100DevsDaily/sortTheOdd.js
@@ -3,6 +3,7 @@ function sortArray(arr){
     let odds=arr.filter(e=>e%2!=0).sort((a,b)=>a-b)
     return arr.map(e=>e%2==0?e:odds.shift())
 }
+//To make more efficient: first, sort with (b-a) to descending order, then use pop() instead of shift(), now it's linear time
 
 console.log(sortArray([7, 1]), [1, 7])
 console.log(sortArray([5, 8, 6, 3, 4]), [3, 8, 6, 5, 4])
