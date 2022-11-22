7937566ba92615b472660933b2de571d3edcd652
BrianAndrewOneil <brian.andrew.oneil@gmail.com>
2022-11-22 15:40:12 -0500

    code wars

---
+++ 6kyu/isANumberPrime.js
@@ -0,0 +1,14 @@
+function isPrime(n)
+{
+    if (n <= 1)
+        return false;
+    if (n == 2 || n == 3)
+        return true;
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+    // Iterate i by (i+6)
+    for (var i = 5; i <= Math.sqrt(n); i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    return true;
+}
