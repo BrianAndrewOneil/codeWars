d9b21c5216ec8d8bc3524262639d09140d136f5a
BrianAndrewOneil <brian.andrew.oneil@gmail.com>
2022-12-05 18:13:19 -0500

    code wars

--- 100DevsDaily/countCharactersInYourString.js
+++ 100DevsDaily/countCharactersInYourString.js
@@ -1,17 +1,21 @@
-function count (string) {  
-    class letter{
-        constructor(name){
-            this.name = name
-            this.value = 1
+//Count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+//What if the string is empty? Then the result should be empty object literal, {}.
+
+function count (str) {  
+    //create empty object
+    //loop thru string. If letter isn't in obj, add it with value of 1
+    //If letter is in obj, update value by 1.
+    let countObj ={}
+    for (const c of str){
+        if(countObj[c]){
+            countObj[c]+=1
+        }else{
+            countObj[c]=1
         }
     }
-    let ans={}
-    for (l of string){
-        if (ans.includes(l)) l.value+=1
-        else {let l=new letter('l')}
-    }
-    return ans;
+    return countObj
 }
 
 console.log(count("aba"), { a: 2, b: 1 })
+console.log(count("abba"), { a: 2, b: 2 })
 console.log(count(""), {})
