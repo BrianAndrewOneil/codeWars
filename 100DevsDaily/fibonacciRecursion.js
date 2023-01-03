146cb5b5ce3a0f7034b4ffc1d6ab645a3d22a739
unknown <brian.andrew.oneil@gmail.com>
2023-01-03 07:26:48 -0500

    code wars

---
+++ 100DevsDaily/fibonacciRecursion.js
@@ -0,0 +1,15 @@
+//Given n, return that value of the fib series. Use recursion this time.
+function fibonacci(n){
+    let ans=[0,1]
+    for(let i=2;i<=n;i++){
+        ans.push(ans[i-2]+ans[i-1])
+    }
+    return ans[n]
+} 
+
+console.log(fibonacci(1), 1)
+console.log(fibonacci(4), 3)
+console.log(fibonacci(8), 21)
+
+
+//0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
