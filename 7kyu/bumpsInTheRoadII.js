61bb0fec5d461d7e782673b4b1f2f8f08889d4f9
BrianAndrewOneil <brian.andrew.oneil@gmail.com>
2023-01-07 18:00:00 -0500

    code wars

---
+++ 7kyu/bumpsInTheRoadII.js
@@ -0,0 +1,7 @@
+//Given a string showing either flat road (_) or bumps (n). 
+//You are able to reach home safely by encountering 15 bumps or less.
+
+let bump=n=>n.split('').filter(e=>e=='n').length<15
+
+console.log(bump('___________________nnnnnn_nnnnnnnnnnnnnnnnnnnn'))
+console.log(bump('___________________nnnnnn_nn'))
