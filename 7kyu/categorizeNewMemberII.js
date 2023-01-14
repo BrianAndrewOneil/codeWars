fd7184f057fbaac15dd62d5ffc0909f585bb9ba5
BrianAndrewOneil <brian.andrew.oneil@gmail.com>
2023-01-14 18:27:09 -0500

    code wars

---
+++ 7kyu/categorizeNewMemberII.js
@@ -0,0 +1,6 @@
+//Given a list of pairs, return whether each member is a senior or open. Senior=at least 55 years old, handicap greater than 7. All others in open.
+let openOrSenior=data=>data.map(arr=>arr[0]>54&&arr[1]>7?'Senior':'Open')
+
+
+console.log(openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]]),['Open', 'Senior', 'Open', 'Senior'])
+console.log(openOrSenior([[3, 12],[55,1],[91, -2],[53, 23]]),['Open', 'Open', 'Open', 'Open'])
