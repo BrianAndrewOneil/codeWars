6fbfffea3e1cf68c0e6f18eeef76092798038963
BrianAndrewOneil <brian.andrew.oneil@gmail.com>
2023-03-05 08:03:46 -0500

    code wars

---
+++ 6kyu/camelCaseMethod.js
@@ -0,0 +1,13 @@
+//Return the input with all words must have their first letter capitalized without spaces.
+//https://www.codewars.com/kata/587731fda577b3d1b0001196/train/javascript
+
+String.prototype.camelCase=function(){
+    if (this.length===0) return "oops"
+    return this.valueOf().split(' ').map(w=>w.trim()).map(w=>w[0].toUpperCase()+w.slice(1)).join('')
+}
+
+console.log("test case".camelCase(), "TestCase");
+console.log("camel Case method".camelCase(), "CamelCaseMethod");
+console.log("say hello".camelCase(), "SayHello");
+console.log("camel case word".camelCase(), "CamelCaseWord");
+console.log("".camelCase(), "");
