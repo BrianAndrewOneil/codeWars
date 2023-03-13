44a20349a2999315a71a6f2d40d49e3cda5e387b
BrianAndrewOneil <brian.andrew.oneil@gmail.com>
2023-03-13 18:02:11 -0400

    code wars

--- 6kyu/longestPalindrome.js
+++ 6kyu/longestPalindrome.js
@@ -1,18 +1,27 @@
 //Find the length of the longest substring in the given string s that is the same in reverse. 
+//https://www.codewars.com/kata/54bb6f887e5a80180900046b/train/javascript
 longestPalindrome=function(s){
+    function isPal(word){
+        for(i=0;i<word.length/2;i++){
+          if(word[i]!==word[word.length-i-1]) return false
+          }
+          return true
+    }
+    
     let ans=0
     for (let i=0;i<s.length;i++){
-        for(let h=i;h<=s.length;h++){
+        if(s.slice(i).length<ans)break
+        for(let h=s.length;h>i;h--){
             let word=s.slice(i,h)
-            if (word===word.split('').reverse().join('')){
+            //console.log(word, word.length, ans)
+            if(word.length<=ans) break
+            if(isPal(word)){
                 word.length>ans ? ans=word.length : ans=ans
             }
         }
     }
     return ans
   }
-
-
 
 
 console.log(longestPalindrome("a"), 1)
