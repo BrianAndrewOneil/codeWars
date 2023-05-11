7d84767a4ff1c0a8b5d405f41b5efa4f4e95da6a
BrianAndrewOneil <brian.andrew.oneil@gmail.com>
2023-05-11 08:09:53 -0400

    code wars

---
+++ 6kyu/salesmansTravel.js
@@ -0,0 +1,32 @@
+//take two parameters r (addresses' list of all clients' as a string) and zipcode and returns a string in the following format:
+//zipcode:street and town,street and town,.../house number,house number,...
+//The street numbers must be in the same order as the streets where they belong.
+
+function travel (r,zipcode){
+    let list = r.split(',').filter(address=>address.slice(-8)===zipcode)
+    let nums = list.map(i=>i.split(' ')[0])
+    let streets = list.map(i=>i.slice(0,-9)).map(i=>i.split(' '))
+    streets = streets.map(x=>x.filter((c,i,a)=>i!=0).join(' ')).join(',')
+    return (`${zipcode}:${streets}/${nums}`)
+    
+}
+
+const ad = "123 Main Street St. Louisville OH 43071,432 Main Long Road St. Louisville OH 43071,786 High Street Pollocksville NY 56432,"
+  + "54 Holy Grail Street Niagara Town ZP 32908,3200 Main Rd. Bern AE 56210,1 Gordon St. Atlanta RE 13000,"
+  + "10 Pussy Cat Rd. Chicago EX 34342,10 Gordon St. Atlanta RE 13000,58 Gordon Road Atlanta RE 13000,"
+  + "22 Tokyo Av. Tedmondville SW 43098,674 Paris bd. Abbeville AA 45521,10 Surta Alley Goodtown GG 30654,"
+  + "45 Holy Grail Al. Niagara Town ZP 32908,320 Main Al. Bern AE 56210,14 Gordon Park Atlanta RE 13000,"
+  + "100 Pussy Cat Rd. Chicago EX 34342,2 Gordon St. Atlanta RE 13000,5 Gordon Road Atlanta RE 13000,"
+  + "2200 Tokyo Av. Tedmondville SW 43098,67 Paris St. Abbeville AA 45521,11 Surta Avenue Goodtown GG 30654,"
+  + "45 Holy Grail Al. Niagara Town ZP 32918,320 Main Al. Bern AE 56215,14 Gordon Park Atlanta RE 13200,"
+  + "100 Pussy Cat Rd. Chicago EX 34345,2 Gordon St. Atlanta RE 13222,5 Gordon Road Atlanta RE 13001,"
+  + "2200 Tokyo Av. Tedmondville SW 43198,67 Paris St. Abbeville AA 45522,11 Surta Avenue Goodville GG 30655,"
+  + "2222 Tokyo Av. Tedmondville SW 43198,670 Paris St. Abbeville AA 45522,114 Surta Avenue Goodville GG 30655,"
+  + "2 Holy Grail Street Niagara Town ZP 32908,3 Main Rd. Bern AE 56210,77 Gordon St. Atlanta RE 13000"
+
+console.log(travel(ad, "AA 45522"), "AA 45522:Paris St. Abbeville,Paris St. Abbeville/67,670");
+console.log(travel(ad, "EX 34342"), "EX 34342:Pussy Cat Rd. Chicago,Pussy Cat Rd. Chicago/10,100");
+console.log(travel(ad, "EX 34345"), "EX 34345:Pussy Cat Rd. Chicago/100");
+console.log(travel(ad, "AA 45521"), "AA 45521:Paris bd. Abbeville,Paris St. Abbeville/674,67");
+console.log(travel(ad, "AE 56215"), "AE 56215:Main Al. Bern/320");
+console.log(travel(ad, ""), ":/");
