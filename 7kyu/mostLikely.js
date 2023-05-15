64c3e609cdf91b5de3f52dd997d8a5729eeff1dd
BrianAndrewOneil <brian.andrew.oneil@gmail.com>
2023-05-15 08:01:38 -0400

    code wars

---
+++ 7kyu/mostLikely.js
@@ -0,0 +1,6 @@
+//Compare two probabilities, returning true if the first one is most likely otherwise false.
+
+mostLikely=(prob1,prob2)=>(prob1.split(':')[0]/prob1.split(':')[1]) > (prob2.split(':')[0]/prob2.split(':')[1])
+
+console.log(mostLikely('1:3','1:2'),false)
+console.log(mostLikely('1:5','1:10'),true)
