05f60343e0676ec63375d51da6f8f83e906ddfb8
BrianAndrewOneil <brian.andrew.oneil@gmail.com>
2023-05-15 08:04:15 -0400

    code wars

--- 7kyu/mostLikely.js
+++ 7kyu/mostLikely.js
@@ -2,5 +2,9 @@
 
 mostLikely=(prob1,prob2)=>(prob1.split(':')[0]/prob1.split(':')[1]) > (prob2.split(':')[0]/prob2.split(':')[1])
 
+<<<<<<< HEAD
 console.log(mostLikely('1:3','1:2'),false)
 console.log(mostLikely('1:5','1:10'),true)
+=======
+console.log(mostLikely('1:3','1:2'),false)
+>>>>>>> f7cbec0451d2a8458459583552d3c42cf9f05fd9
