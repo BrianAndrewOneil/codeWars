2cb6553f607ac16c8c57883507c9e6c0eefb9436
BrianAndrewOneil <brian.andrew.oneil@gmail.com>
2023-05-18 23:28:55 -0400

    code wars

---
+++ 5kyu/paginationHelper.js
@@ -0,0 +1,47 @@
+class PaginationHelper {
+	constructor(collection, itemsPerPage) {
+	// The constructor takes in an array of items and a integer indicating how many items fit within a single page
+    this.collection = collection
+    this.itemsPerPage = itemsPerPage
+	}
+	itemCount() {
+	// returns the number of items within the entire collection
+    return this.collection.length
+	}
+	pageCount() {
+	// returns the number of pages
+    return Math.ceil(this.collection.length/this.itemsPerPage)
+	}
+	pageItemCount(pageIndex) {
+	// returns the number of items on the current page. page_index is zero based.
+	// this method should return -1 for pageIndex values that are out of range
+        if (pageIndex+1>this.pageCount()) return -1
+        else if (pageIndex+1<this.pageCount()) return this.itemsPerPage
+        else return this.itemCount()%this.itemsPerPage
+        
+	}
+	pageIndex(itemIndex) {
+	// determines what page an item is on. Zero based indexes
+	// this method should return -1 for itemIndex values that are out of range
+    if (itemIndex+1>this.itemCount()||itemIndex<0) return -1
+    else return Math.ceil(this.itemsPerPage/(itemIndex+1))
+	}
+}
+
+const collection = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+const helper = new PaginationHelper(collection, 10)
+
+console.log(helper.pageCount(), 3);
+console.log(helper.itemCount(), 24);
+
+console.log(helper.pageItemCount(1), 10);
+console.log(helper.pageItemCount(2), 4);
+console.log(helper.pageItemCount(3), -1);
+console.log(helper.pageIndex(40), -1);
+
+console.log(helper.pageIndex(22), 2);
+console.log(helper.pageIndex(3), 0);
+console.log(helper.pageIndex(0), 0);
+console.log(helper.pageIndex(-1), -1);
+console.log(helper.pageIndex(-23), -1);
+console.log(helper.pageIndex(-15), -1);
