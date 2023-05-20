08e07e9a71df34435c58990d0ffaf61c21426cde
BrianAndrewOneil <brian.andrew.oneil@gmail.com>
2023-05-19 20:07:31 -0400

    code wars

--- 5kyu/paginationHelper.js
+++ 5kyu/paginationHelper.js
@@ -13,18 +13,16 @@ class PaginationHelper {
     return Math.ceil(this.collection.length/this.itemsPerPage)
 	}
 	pageItemCount(pageIndex) {
-	// returns the number of items on the current page. page_index is zero based.
-	// this method should return -1 for pageIndex values that are out of range
-        if (pageIndex+1>this.pageCount()) return -1
+	// returns the number of items on the current page. page_index is zero based. Return -1 for pageIndex values that are out of range
+        if (pageIndex+1>this.pageCount()||pageIndex<0) return -1
         else if (pageIndex+1<this.pageCount()) return this.itemsPerPage
         else return this.itemCount()%this.itemsPerPage
         
 	}
 	pageIndex(itemIndex) {
-	// determines what page an item is on. Zero based indexes
-	// this method should return -1 for itemIndex values that are out of range
+	// determines what page an item is on. Zero based indexes. Return -1 for itemIndex values that are out of range
     if (itemIndex+1>this.itemCount()||itemIndex<0) return -1
-    else return Math.ceil(this.itemsPerPage/(itemIndex+1))
+    else return Math.floor((itemIndex+1)/this.itemsPerPage)
 	}
 }
 
