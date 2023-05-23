22a5a2fed5b40b52a9616eed3bffd6e87775b92d
BrianAndrewOneil <brian.andrew.oneil@gmail.com>
2023-05-22 20:33:13 -0400

    code wars

--- 5kyu/paginationHelper.js
+++ 5kyu/paginationHelper.js
@@ -1,3 +1,4 @@
+//https://www.codewars.com/kata/515bb423de843ea99400000a/train/javascript
 class PaginationHelper {
 	constructor(collection, itemsPerPage) {
 	// The constructor takes in an array of items and a integer indicating how many items fit within a single page
@@ -22,24 +23,29 @@ class PaginationHelper {
 	pageIndex(itemIndex) {
 	// determines what page an item is on. Zero based indexes. Return -1 for itemIndex values that are out of range
     if (itemIndex+1>this.itemCount()||itemIndex<0) return -1
-    else return Math.floor((itemIndex+1)/this.itemsPerPage)
+    else return Math.floor((itemIndex)/this.itemsPerPage)
 	}
 }
 
 const collection = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
 const helper = new PaginationHelper(collection, 10)
 
-console.log(helper.pageCount(), 3);
-console.log(helper.itemCount(), 24);
+// console.log(helper.pageCount(), 3);
+// console.log(helper.itemCount(), 24);
 
 console.log(helper.pageItemCount(1), 10);
 console.log(helper.pageItemCount(2), 4);
 console.log(helper.pageItemCount(3), -1);
-console.log(helper.pageIndex(40), -1);
+//console.log(helper.pageItemCount(5), 5);
 
-console.log(helper.pageIndex(22), 2);
-console.log(helper.pageIndex(3), 0);
-console.log(helper.pageIndex(0), 0);
-console.log(helper.pageIndex(-1), -1);
-console.log(helper.pageIndex(-23), -1);
-console.log(helper.pageIndex(-15), -1);
+
+// console.log(helper.pageIndex(13), 1);
+// console.log(helper.pageIndex(40), -1);
+// console.log(helper.pageIndex(5), 0);
+// console.log(helper.pageIndex(15), 1);
+// console.log(helper.pageIndex(22), 2);
+// console.log(helper.pageIndex(3), 0);
+// console.log(helper.pageIndex(0), 0);
+// console.log(helper.pageIndex(-1), -1);
+// console.log(helper.pageIndex(-23), -1);
+// console.log(helper.pageIndex(-15), -1);
