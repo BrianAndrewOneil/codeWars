09b3ecfdbc45ef14b044a2c463886647dcc5e6a9
BrianAndrewOneil <brian.andrew.oneil@gmail.com>
2023-05-27 20:00:42 -0400

    code wars

---
+++ 3kyu/battleshipFieldValidator.js
@@ -0,0 +1,22 @@
+//https://www.codewars.com/kata/52bb6539a4cf1b12d90005b7/train/javascript
+//Test that the board is set up properly. 
+//Per player: Onee battleship (4 cells), 2 cruisers (3), 3 destroyers (2) and 4 submarines (1).
+//Ships cannot overlap or be in contact with any other ship.
+function validateBattlefield(field) {
+    // write your magic here
+}
+
+
+
+console.log(validateBattlefield([
+    [1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+    [1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+    [1, 0, 1, 0, 1, 1, 1, 0, 1, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+]), true)
