54197add35ff318ddc3cd13d8d51b0bb0041216e
BrianAndrewOneil <brian.andrew.oneil@gmail.com>
2023-06-03 08:47:36 -0400

    code wars

--- 6kyu/giveMeADiamond.js
+++ 6kyu/giveMeADiamond.js
@@ -3,18 +3,18 @@
 //Trailing spaces should be removed, and every line must be terminated with a newline character (\n).
 function diamond(n){
     if (n<=0||n%2==0) return null
-    let space = Math.floor(n/2), star = 1 //Math.ceil(n/2)
-    console.log(star)
+    let space = Math.floor(n/2), star = 1
     let ans = []
     while (space>=0){
-        ans.push(`${' '.repeat(space)}${'*'.repeat(star)}`)
+        ans.push(`${' '.repeat(space)}${'*'.repeat(star)}\n`)
         space--
         star+=2
     }
-    return ans
+    let ans2 = ans.slice(0,ans.length-1).reverse()
+    return (ans.concat(ans2).join(''))
 }
 
-console.log(diamond(1), "*\n")
+console.log(diamond(1),"*\n")
 console.log(diamond(3), " *\n***\n *\n")
 console.log(diamond(5), "  *\n ***\n*****\n ***\n  *\n")
 console.log(diamond(0), "null")
