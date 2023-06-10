60316b1882e8f902b77f3081c0785c5ab41387f7
BrianAndrewOneil <brian.andrew.oneil@gmail.com>
2023-06-10 18:32:16 -0400

    code wars

---
+++ 6kyu/squareDigitsSequence.js
@@ -0,0 +1,13 @@
+//Consider a sequence of numbers a0, a1, ..., an, in which an element is equal to the sum of squared digits of the previous element. 
+//The sequence ends once an element that has already been in the sequence appears again.
+//Given the first element a0, find the length of the sequence.
+
+function squareDigitsSequence(a0) {
+    //coding and coding...
+    
+    
+}
+
+console.log( squareDigitsSequence(16) , 9)
+console.log( squareDigitsSequence(103) , 4)
+console.log( squareDigitsSequence(1) , 2)
