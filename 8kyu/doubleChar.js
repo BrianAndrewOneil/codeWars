99e44b6a5233a563fe566d9674f1b99790f19f40
BrianAndrewOneil <brian.andrew.oneil@gmail.com>
2023-07-09 07:03:20 -0400

    code wars

---
+++ 8kyu/doubleChar.js
@@ -0,0 +1,9 @@
+//Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+const doubleChar = (word) => word.split('').map(l=>l+l).join('')
+
+console.log(doubleChar("abcd"), "aabbccdd");
+console.log(doubleChar("Adidas"), "AAddiiddaass");
+console.log(doubleChar("1337"), "11333377");
+console.log(doubleChar("illuminati"), "iilllluummiinnaattii");
+console.log(doubleChar("123456"), "112233445566");
+console.log(doubleChar("%^&*("), "%%^^&&**((");
