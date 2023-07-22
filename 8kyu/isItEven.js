5e3649549d3e8ba8be3c0e687b74475e6648e9be
BrianAndrewOneil <brian.andrew.oneil@gmail.com>
2023-07-22 07:01:40 -0400

    code wars

---
+++ 8kyu/isItEven.js
@@ -0,0 +1,8 @@
+const testEven = n => n%2===0
+
+
+console.log(testEven(0), true, "testEven for 0");
+console.log(testEven(0.5), false, "testEven for 0.5");
+console.log(testEven(1), false, "testEven for 1");
+console.log(testEven(2), true, "testEven for 2");
+console.log(testEven(-4), true, "testEven for 2");
