54a3c9340c9af0d2cd43fd3d73c726a1d2fde079
BrianAndrewOneil <brian.andrew.oneil@gmail.com>
2023-07-24 09:22:35 -0400

    code wars

---
+++ 8kyu/grasshopperPersonalizedMessage.js
@@ -0,0 +1,3 @@
+//Return a personalized message from two inputs, name and owner. If name===owner, "hello boss", otherwise, "hello guest."
+
+const greet = (n,o) => n===o ? "Hello boss" : "Hello guest"
