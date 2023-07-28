425fe6c9c73bd0d94b47d934c5de1e0345deaef7
BrianAndrewOneil <brian.andrew.oneil@gmail.com>
2023-07-28 07:15:55 -0400

    code wars

--- 8kyu/whatsTheRealFloor.js
+++ 8kyu/whatsTheRealFloor.js
@@ -3,9 +3,10 @@
 //In case of above 13, they move down by two because there are two omitted numbers below them.
 //Basements (negatives) stay the same as the universal level.
 
-const getRealFloor = n => n<0 ? n : n < 13 ? n-1 : n-2
+const getRealFloor = n => n<=0 ? n : n < 13 ? n-1 : n-2
 
 console.log(getRealFloor( 1),  0);
 console.log(getRealFloor( 5),  4);
 console.log(getRealFloor(15), 13);
 console.log(getRealFloor(-2), -2);
+console.log(getRealFloor(0), 0);
