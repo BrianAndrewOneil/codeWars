21ddfb828fa061f7bb25478789d02cb3a4d2cbbc
Brian O'Neil <brian.andrew.oneil@gmail.com>
2023-10-17 07:54:25 -0400

    code wars

--- 8kyu/transportationOnVacation.js
+++ 8kyu/transportationOnVacation.js
@@ -6,13 +6,16 @@ function rentalCarCost(d,discount=0) {
     return (d*40-discount)
 }
 
-console.log(rentalCarCost(1), 40);
-console.log(rentalCarCost(2), 80);  
-console.log(rentalCarCost(3), 100);
-console.log(rentalCarCost(4), 140);
-console.log(rentalCarCost(5), 180);
-console.log(rentalCarCost(6), 220);
-console.log(rentalCarCost(7), 230);
-console.log(rentalCarCost(8), 270);
-console.log(rentalCarCost(9), 310);
-console.log(rentalCarCost(10), 350);    
+const rentalCarCost1 = d => d>=7 ? d*40-50 : d>=3 ? d*40-20 : d*40
+
+
+console.log(rentalCarCost1(1), 40);
+console.log(rentalCarCost1(2), 80);  
+console.log(rentalCarCost1(3), 100);
+console.log(rentalCarCost1(4), 140);
+console.log(rentalCarCost1(5), 180);
+console.log(rentalCarCost1(6), 220);
+console.log(rentalCarCost1(7), 230);
+console.log(rentalCarCost1(8), 270);
+console.log(rentalCarCost1(9), 310);
+console.log(rentalCarCost1(10), 350);    
