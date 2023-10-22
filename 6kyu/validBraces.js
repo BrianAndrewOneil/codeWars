6929915bfb358843d79b1a1adb06a883ca5bab93
Brian O'Neil <brian.andrew.oneil@gmail.com>
2023-10-22 11:26:02 -0400

    code wars

--- 6kyu/validBraces.js
+++ 6kyu/validBraces.js
@@ -1,59 +1,36 @@
 //https://www.codewars.com/kata/5277c8a221e209d3f6000b56/train/javascript
 //Given a string of brackets, braces, and curly braces, if they are in a valid order return true, otherwise false.
 function validBraces(braces){
-    var currentOpen
-    let map = {
-        ")" : 0,
-        "}" : 0,
-        "]" : 0
-    }
+    let stack = []
     for (var char of braces){
-        if (char==="}") {
-            if (currentOpen!='{') return false
-            currentOpen=currentOpen.slice(0,-1)
-            map["}"]-=1
+        if (char==='['||char==='('||char==='{') stack.push(char)
+        else if (char===')'){
+            if (stack[stack.length-1]!='(') return false
+            stack.pop()
+        }else if (char===']'){
+            if (stack[stack.length-1]!='[') return false
+            stack.pop()
+        }else if (char==='}'){
+            if (stack[stack.length-1]!='{') return false
+            stack.pop()
         }
-        if (char===")") {
-            if (currentOpen!='(') return false
-            currentOpen=currentOpen.slice(0,-1)
-            map[")"]-=1
-        }
-        if (char==="]") {
-            if (currentOpen!='[') return false
-            currentOpen=currentOpen.slice(0,-1)
-            map["]"]-=1
-        }
-        if (char==="{") {
-            map["}"]+=1
-            currentOpen+='{'
-        }
-        if (char==="(") {
-            map[")"]+=1
-            currentOpen+='('
-        }
-        if (char==="[") {
-            map["]"]+=1
-            currentOpen+='{'
-        }
-        if (map[char]<0) return false
     }
-    //return map
-    return(Object.values(map).reduce((acc,c)=>acc+c,0)===0?true:false)
+    return stack.length===0
 }
 
-// console.log(validBraces("()))"), false);
-// console.log(validBraces("()"), true);
-// console.log(validBraces("[]"), true);
-// console.log(validBraces("{}"), true);
-// console.log(validBraces("(){}[]"), true);
-// console.log(validBraces("([{}])"), true);
+console.log(validBraces("()))"), false);
+console.log(validBraces("()"), true);
+console.log(validBraces("[]"), true);
+console.log(validBraces("{}"), true);
+console.log(validBraces("(){}[]"), true);
+console.log(validBraces("([{}])"), true);
 console.log(validBraces("(}"), false);
 console.log(validBraces("[(])"), false);
-// console.log(validBraces("({})[({})]"), true);
-// console.log(validBraces("(})"), false);
-// console.log(validBraces("(({{[[]]}}))"), true);
-// console.log(validBraces("{}({})[]"), true);
-// console.log(validBraces(")(}{]["), false);
-// console.log(validBraces("())({}}{()][]["), false);
-// console.log(validBraces("(((({{"), false);
-// console.log(validBraces("}}]]))}])"), false);
+console.log(validBraces("({})[({})]"), true);
+console.log(validBraces("(})"), false);
+console.log(validBraces("(({{[[]]}}))"), true);
+console.log(validBraces("{}({})[]"), true);
+console.log(validBraces(")(}{]["), false);
+console.log(validBraces("())({}}{()][]["), false);
+console.log(validBraces("(((({{"), false);
+console.log(validBraces("}}]]))}])"), false);
