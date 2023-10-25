5bd636a190be273ffffd29033c53f8b9d355a444
Brian O'Neil <brian.andrew.oneil@gmail.com>
2023-10-25 07:13:23 -0400

    code wars

--- 6kyu/portionOfAnArray.js
+++ 6kyu/portionOfAnArray.js
@@ -2,10 +2,11 @@
 //Your task is to return n portion of the array (a) starting from position (i). 
 //If i is negative, start counting from the end of the array and work backwards.
 function portion(a,i,n){
-    if (i>0 && i+n<=a.length-1){
+    if (i>0 && i+n<=a.length){
         return a.slice(i,(i+n))
-    }else if (i<0)
-        return a.slice(i,)
+    }else if (i<0 && i+a.length-n>=0){
+        return a.slice(i+a.length-n,i)
+    }else return -1
 }
 
 console.log(portion([1,2,3,4],1,2),[2,3])
