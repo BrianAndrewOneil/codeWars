91ad4c6f5ad061c618290d9e4ac3c0725f583aac
Brian O'Neil <brian.andrew.oneil@gmail.com>
2023-11-27 17:05:32 -0500

    code wars

--- 7kyu/sexyPrimes.js
+++ 7kyu/sexyPrimes.js
@@ -1,11 +1,14 @@
 //Two numbers are "sexy primes" if they are both prime and 6 apart.
 function sexy_prime(x, y){
     let pair = [x,y].sort((a,b)=>a-b)
-    var prime = true
     if (pair[0]<=1) return false
-    for (let n=2; n<=pair[1]; n++){
-        if ()
+    for (let n=2; n<pair[0]; n++){
+        if (pair[0]%n===0) return false
     }
+    for (let n=2; n<pair[1]; n++){
+        if (pair[1]%n===0) return false
+    }
+    return pair[1]-pair[0]===6
 }
 
 console.log(sexy_prime(5, 11),true)
