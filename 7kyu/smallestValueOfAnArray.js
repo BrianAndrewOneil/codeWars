627d44d6d3c2b5773befbaa55287b3799b5bbd73
Brian O'Neil <brian.andrew.oneil@gmail.com>
2023-12-14 08:13:39 -0500

    code wars

---
+++ 7kyu/smallestValueOfAnArray.js
@@ -0,0 +1,11 @@
+//Write a function that can return the smallest value of an array or the index of that value. 
+//The function's 2nd parameter will tell whether it should return the value or the index.
+//https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
+
+function min(arr, toReturn) {
+    return ( toReturn==='value' ? arr.sort((a,b)=>a-b)[0] : arr.indexOf(arr.sort((a,b)=>a-b)[0]))
+}
+
+console.log(min([1,2,3,4,5,0,-3], 'value'), 1)
+console.log(min([1,2,3,4,5,0], 'index'), 0)
+console.log(min([ 500, 250, 750, 5000, 1000, 230 ], 'index'), 5)
