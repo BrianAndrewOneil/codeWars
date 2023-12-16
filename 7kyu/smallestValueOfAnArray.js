7abf1f0f0b03104b4806fe849dc2be9fa37831f3
Brian O'Neil <brian.andrew.oneil@gmail.com>
2023-12-16 11:32:28 -0500

    code wars

--- 7kyu/smallestValueOfAnArray.js
+++ 7kyu/smallestValueOfAnArray.js
@@ -2,7 +2,7 @@
 //The function's 2nd parameter will tell whether it should return the value or the index.
 //https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
 
-const min = (arr, toReturn) => toReturn==='value' ? arr.map(x=>x).sort((a,b)=>a-b)[0] : arr.indexOf(arr.map(x=>x).sort((a,b)=>a-b)[0])
+const min = (arr, toReturn) => toReturn==='value' ? Math.min(...arr) : arr.indexOf(Math.min(...arr))
 
 console.log(min([1,2,3,4,5,0,-3], 'value'), 1)
 console.log(min([1,2,3,4,5,0], 'index'), 0)
