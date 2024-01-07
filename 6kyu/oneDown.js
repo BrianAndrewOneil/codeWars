053efa90e77d4545b5868ff0393250da56ff4f65
Brian O'Neil <brian.andrew.oneil@gmail.com>
2024-01-07 14:16:56 -0500

    code wars

--- 6kyu/oneDown.js
+++ 6kyu/oneDown.js
@@ -2,16 +2,17 @@
 //https://www.codewars.com/kata/56419475931903e9d1000087/train/javascript
 
 function oneDown(str) {
-if (typeof(str)!='string') return "Input is not a string"
-let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-str=str.split(' ').map((l,i,a)=> {
-    const indexVal = 
-})
+    if (typeof(str)!='string') return "Input is not a string"
+    let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    let answer = []
+    str=str.split(' ').map((word,i,a) => {
+        let newWord = (word.split('').map((letter) => letter==="A" ? letter=letters.length : letter=letters.indexOf(letter)))
+        newWord = newWord.map(letter => letters.charAt(letter-1)).join('')
+        answer.push(newWord)
+    })
+    return answer.join(' ')
 }
 
-//charCodeAt()
-//fromCharCode(#)
-
-console.log(oneDown("Ifmmp"), "Hello");
+console.log(oneDown("IfmmpA"), "Helloz");
 console.log(oneDown("Uif usjdl up uijt lbub jt tjnqmf"), "The trick to this kata is simple");
 console.log(oneDown(45), "Input is not a string");
