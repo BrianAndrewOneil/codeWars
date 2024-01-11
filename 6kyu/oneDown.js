54dbfd82fa07beb8a926d8959343461e5ea5721e
Brian O'Neil <brian.andrew.oneil@gmail.com>
2024-01-11 06:55:26 -0500

    code wars

--- 6kyu/oneDown.js
+++ 6kyu/oneDown.js
@@ -1,19 +1,17 @@
 //Decode, letters are one down. Return "Input is not a string" if input isn't a string.
-//https://www.codewars.com/kata/56419475931903e9d1000087/train/javascript
 
 function oneDown(str) {
     if (typeof(str)!='string') return "Input is not a string"
-    let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    let letters = ".,:;!?ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     let answer = []
     str=str.split(' ').map((word,i,a) => {
-        let newWord = (word.split('').map((letter) => letter==="A" ? letter=letters.length : letters.indexOf(letter)>0 ? letter=letters.indexOf(letter) : letter))
-        console.log(newWord)
-        newWord = newWord.map(letter => letters.charAt(letter-1)).join('')
+        let newWord = (word.split('').map((letter) => letter==="A" ? letter=letters.length : letter=letters.indexOf(letter)))
+        newWord = newWord.map(letter => letter<6 ? letters[letter] : letters.charAt(letter-1)).join('')
         answer.push(newWord)
     })
     return answer.join(' ')
 }
 
-console.log(oneDown("IfmmpA;"), "Helloz;");
-// console.log(oneDown("Uif usjdl up uijt lbub jt tjnqmf!"), "The trick to this kata is simple!");
-// console.log(oneDown(45), "Input is not a string");
+console.log(oneDown("IfmmpA;."), "Helloz;.");
+console.log(oneDown("Uif usjdl up uijt lbub jt tjnqmf!"), "The trick to this kata is simple!");
+console.log(oneDown(45), "Input is not a string");
