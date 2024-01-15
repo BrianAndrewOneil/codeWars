58ffc49c954c31de432b53946fc299f3a9fb7834
Brian O'Neil <brian.andrew.oneil@gmail.com>
2024-01-15 17:06:59 -0500

    code wars

--- 6kyu/gettingMAD.js
+++ 6kyu/gettingMAD.js
@@ -1,11 +1,16 @@
 //Given an array of integers with at least 2 elements: a1, a2, a3, a4, ... aN
 //The absolute difference between two array elements ai and aj, where i != j, is the absolute value of ai - aj.
 //Return the minimum absolute difference (MAD) between any two elements in the array.
+
 function gettingMad(array) {
     let ans = Math.abs(array[0]-array[1])
     for (let i=0; i<array.length; i++){
-        for (let j=0; j<array.length-1; j++){
-            if (Math.abs(array[i]-array[j] < ans)) ans=Math.abs(array[i]-array[j])
+        for (let j=0; j<array.length; j++){
+            if (i!=j) {
+                if (Math.abs(array[i]-array[j]) < ans) {
+                    ans=Math.abs(array[i]-array[j])
+                }
+            }
         }
     }
     return ans;
