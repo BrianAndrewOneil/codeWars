62944aa0e789fc20b30d6ed35ec0b8b7da79e160
Brian O'Neil <brian.andrew.oneil@gmail.com>
2024-01-28 09:37:23 -0500

    code wars

---
+++ 6kyu/throwingDarts.js
@@ -0,0 +1,21 @@
+//Scoring specifications:
+//0 points - radius above 10, 5 points - radius between 5 and 10 inclusive, 10 points - radius less than 5
+//If all radii are less than 5, award 100 BONUS POINTS!
+//https://www.codewars.com/kata/525dfedb5b62f6954d000006/train/javascript
+
+function scoreThrows(radii){
+    let score = 0, bonus=true
+    for (let n of radii){
+        n>10?score+=0 : n>4? score+=5 : score +=10
+        if (n>5) bonus=false
+        console.log(score)
+    }
+    return (bonus ? score+100 : score)
+}
+
+// console.log(scoreThrows([1, 5, 11]), 15);
+// console.log(scoreThrows([15, 20, 30, 31, 32, 44, 100]),  0);
+// console.log(scoreThrows([1, 2, 3, 4]), 140);
+// console.log(scoreThrows([]), 0, 'Empty list');
+// console.log(scoreThrows([1, 2, 3, 4, 5, 6, 7, 8, 9]), 65);
+console.log(scoreThrows([0, 5, 10, 10.5, 4.5]),  30);
