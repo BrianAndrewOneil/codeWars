6268a5396f7b26f15c12f2a21f96e81f66d2103e
Brian O'Neil <brian.andrew.oneil@gmail.com>
2024-02-08 10:39:39 -0500

    code wars

---
+++ 5kyu/greedIsGood.js
@@ -0,0 +1,32 @@
+//Score the dice game "greed", where 5 dice are thrown. Scoring rules are:
+// Three 1's => 1000 points
+// Three 6's =>  600 points
+// Three 5's =>  500 points
+// Three 4's =>  400 points
+// Three 3's =>  300 points
+// Three 2's =>  200 points
+// One   1   =>  100 points
+// One   5   =>   50 point
+//https://www.codewars.com/kata/5270d0d18625160ada0000e4/train/javascript
+
+function score( dice ) {
+    let score = 0
+    const map = {}
+    const scoreThrees = { '1': 1000, '2': 200, '3': 300, '4': 400, '5': 500, '6': 600 }
+    for (const num of dice){
+        map[num]=map[num]+1 || 1
+    }
+    //console.log(map)
+    for (const num in map){
+        if (map[num]>2){
+            let numThree = Math.floor(map[num]/3)
+            console.log(`number of triplets for ${num} is: ${numThree}`)
+            //then add to score, and subtract from map[num]
+            
+        }
+    }
+}
+
+console.log( score( [2, 3, 4, 6, 2] ) == 0,   "Should be 0 :-(" );
+console.log( score( [4, 4, 4, 3, 3] ) == 400, "Should be 400" );
+console.log( score( [2, 4, 4, 5, 4] ) == 450, "Should be 450" );
