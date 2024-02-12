a4616a03234efe9c9bde1cace96b67a4b5ec5913
Brian O'Neil <brian.andrew.oneil@gmail.com>
2024-02-11 21:55:20 -0500

    code wars

---
+++ 7kyu/largestFiveDigitNumberInASeries.js
@@ -0,0 +1,7 @@
+//Return the greatest sequence of five consecutive digits found within the number given. 
+//https://www.codewars.com/kata/51675d17e0c1bed195000001/train/javascript
+
+function solution(digits){
+  
+}
+
