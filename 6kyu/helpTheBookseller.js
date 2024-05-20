1b1ead95bb09fbce8ae1988bbd7a1c91bbfd12c6
Brian O'Neil <brian.andrew.oneil@gmail.com>
2024-05-20 18:31:43 -0400

    code wars

---
+++ 6kyu/helpTheBookseller.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/54dc6f5a224c26032800005c/train/javascript
+//A bookseller has lots of books classified in 26 categories labeled A, B, ... Z. Each book has a code c of 3, 4, 5 or more characters. 
+//The 1st character of a code is a capital letter which defines the book category.
+//In the bookseller's stocklist each code c is followed by a space and by a positive integer n (int n >= 0) which indicates the quantity of books of this code in stock.
+//Given a stocklist (e.g. : L) and a list of categories in capital letter, 
+//find all the books of L with codes belonging to each category of M and to sum their quantity according to each category. 
+
+function stockList(listOfArt, listOfCat){
+    // ... 
+}
