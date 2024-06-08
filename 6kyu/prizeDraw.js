2721a0be2bcf34a00e21508f7477d0f1aedc2d56
Brian O'Neil <brian.andrew.oneil@gmail.com>
2024-06-08 12:10:02 -0400

    code wars

--- 6kyu/prizeDraw.js
+++ 6kyu/prizeDraw.js
@@ -14,11 +14,13 @@ function rank(st, we, n) {
         map[name]=(name.toUpperCase().split('').map(letter=>letter.charCodeAt(0)-64).reduce((acc,c)=>acc+c,0)+name.length)*we[weNum]
         weNum++
     }
+    console.log(map)
     let place = (Object.values(map).sort((a,b)=>b-a))[n-1]
-    return place //map[place]
+    return Object.keys(map).find(key => map[key] === place)
 }
-console.log(rank("COLIN,AMANDBA,AMANDAB,CAROL,PauL,JOSEPH", [1, 4, 4, 5, 2, 1], 4), "PauL")
-console.log(rank("Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin", [4, 2, 1, 4, 3, 1, 2], 4), "Benjamin")
-console.log(rank("Lagon,Lily", [1, 5], 2), "Lagon")
-console.log(rank("Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin", [4, 2, 1, 4, 3, 1, 2], 8), "Not enough participants")
+console.log(rank("Elijah,Chloe,Elizabeth,Matthew,Natalie,Jayden", [ 1, 3, 5, 5, 3, 6 ], 2), "Matthew")
+// console.log(rank("COLIN,AMANDBA,AMANDAB,CAROL,PauL,JOSEPH", [1, 4, 4, 5, 2, 1], 4), "PauL")
+// console.log(rank("Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin", [4, 2, 1, 4, 3, 1, 2], 4), "Benjamin")
+// console.log(rank("Lagon,Lily", [1, 5], 2), "Lagon")
+// console.log(rank("Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin", [4, 2, 1, 4, 3, 1, 2], 8), "Not enough participants")
 console.log(rank("", [4, 2, 1, 4, 3, 1, 2], 6), "No participants")
